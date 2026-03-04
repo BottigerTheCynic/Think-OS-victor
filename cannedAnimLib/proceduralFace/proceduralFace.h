ded54b0dcebf90af006352ed932c528850340487
TheBottiger <VoidSecOp@protonmail.com>
2026-03-04 15:55:11 -0600

    actually have more line please

--- cannedAnimLib/proceduralFace/proceduralFace.h
+++ cannedAnimLib/proceduralFace/proceduralFace.h
@@ -272,7 +272,7 @@ inline void ProceduralFace::SetScanlineOpacity(Value opacity)
 
 inline ProceduralFace::Value ProceduralFace::GetScanlineOpacity() const {
 #if PROCEDURALFACE_SCANLINE_FEATURE
-  return 1.0;
+  return 2.0;
 #else
   return 1.0f;
 #endif
